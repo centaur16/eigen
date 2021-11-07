2fe2cc45ee635c3b1950b82ecb3de898a1948b69
dave <centaur16>
2021-11-07 12:01:32 +0000

    Use cursive
    * Add cursive as a cargo dependency
    * Rewrite hello world as a cursive hello world

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+use cursive::views::TextView;
+use cursive::event::Key;
+
 fn main() {
-    println!("Hello, world!");
+    let mut win = cursive::default();
+
+    win.add_global_callback(Key::Esc, |w| w.quit());
+    win.add_layer(TextView::new("Hello, world!"));
+    win.run();
 }
