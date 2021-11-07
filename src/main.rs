e976d1b5e0e39753d7a9183270289e2daf58ed1a
dave <centaur16>
2021-11-07 15:42:38 +0000

    Add some theming to the hello world application

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,16 @@
 use cursive::views::TextView;
 use cursive::event::Key;
+use cursive::theme::Color;
 
 fn main() {
     let mut win = cursive::default();
+
+    win.update_theme(
+        |t| {
+                t.shadow = false;
+                t.palette.set_color("Background", Color::Rgb(10, 10, 10));
+                t.palette.set_color("Primary", Color::Rgb(255, 255, 255));
+            });
 
     win.add_global_callback(Key::Esc, |w| w.quit());
     win.add_layer(TextView::new("Hello, world!"));
