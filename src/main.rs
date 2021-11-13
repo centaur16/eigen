25bb1379b17c86b7baf2c69de6929c86f0cdb643
dave <centaur16>
2021-11-13 11:54:50 +0000

    Add menubar and update theming
    Hodge-podge, don't expect logical atomic commits.

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 use cursive::views::{Dialog, TextView};
 use cursive::event::Key;
+use cursive::menu;
+use cursive::theme::BaseColor::{Black, White};
 use cursive::theme::Color;
 
 fn main() {
@@ -8,12 +10,24 @@ fn main() {
     win.update_theme(
         |t| {
                 t.shadow = false;
-                t.palette.set_color("Background", Color::Rgb(10, 10, 10));
-                t.palette.set_color("View", Color::Rgb(10, 10, 10));
-                t.palette.set_color("Primary", Color::Rgb(255, 255, 255));
+                t.palette.set_color("Background", Color::Dark(Black));
+                t.palette.set_color("View", Color::Dark(Black));
+                t.palette.set_color("Primary", Color::Dark(White));
             });
 
-    win.add_global_callback(Key::Esc, |w| w.quit());
+    win.menubar()
+        .add_subtree(
+            "File",
+            menu::MenuTree::new()
+                .leaf("New", move |s| {
+                    s.add_layer(Dialog::info("New file!"));
+                })
+        );
+
+    win.set_autohide_menu(false);
+
+    win.add_global_callback('q', |w| w.quit());
+    win.add_global_callback(Key::Esc, |w| w.select_menubar());
     win.add_layer(TextView::new("Hello, world!"));
     win.run();
 }
