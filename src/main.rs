b2dd38b7a5664dba69bc2c2e6256f0cd13a17923
Eliot Lim <eliot.lim@gmail.com>
2021-12-07 22:43:55 +0800

    Add rudimentary profile selection menu, update dependencies

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,35 @@
+use std::vec;
 use cursive::views::{Dialog, LinearLayout, Panel, ScrollView, TextArea, TextView};
 use cursive::event::Key;
 use cursive::menu;
 use cursive::theme::BaseColor::{Black, White};
 use cursive::theme::Color;
+use crate::menu::MenuTree;
+
+struct Profile {
+    identifier: String,
+    description: String,
+}
 
 fn main() {
     let mut win = cursive::default();
 
+    let mut profile_menu = MenuTree::new();
+    (0..3)
+        .map(|i| Profile { identifier: format!("profile{}", i), description: format!("Profile {}", i) })
+        .for_each(|profile| {
+            profile_menu.add_leaf(profile.description.clone(), move |s| {
+                s.add_layer(Dialog::info(format!("Selected {}", profile.description.clone())))
+            })
+        });
+
     win.update_theme(
         |t| {
-                t.shadow = false;
-                t.palette.set_color("Background", Color::Dark(Black));
-                t.palette.set_color("View", Color::Dark(Black));
-                t.palette.set_color("Primary", Color::Dark(White));
-            });
+            t.shadow = false;
+            t.palette.set_color("Background", Color::Dark(Black));
+            t.palette.set_color("View", Color::Dark(Black));
+            t.palette.set_color("Primary", Color::Dark(White));
+        });
 
     win.menubar()
         .add_subtree(
@@ -21,7 +37,16 @@ fn main() {
             menu::MenuTree::new()
                 .leaf("New", move |s| {
                     s.add_layer(Dialog::info("New file!"));
+                }),
+        )
+        .add_subtree(
+            "Accounts",
+            menu::MenuTree::new()
+                .leaf("Add Account Profile", move |s| {
+                    s.add_layer(Dialog::info("Adding a new Account profile"));
                 })
+                .delimiter()
+                .subtree("Switch Account Profile", profile_menu),
         );
 
     win.set_autohide_menu(false);
